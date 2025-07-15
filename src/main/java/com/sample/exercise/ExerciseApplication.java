f65a3658a4d77741ab35e5aab8e8b63bd99dd1b6
kkr <kotesh@infotech>
2025-07-14 20:05:32 -0400

    fourth commit

--- src/main/java/com/sample/exercise/ExerciseApplication.java
+++ src/main/java/com/sample/exercise/ExerciseApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ExerciseApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		SpringApplication.run(ExerciseApplication.class, args);
 	}
-	// this is test message
+	// this is test message //
 
 }
