0241d959fc6c1d772e42fd4a964722a8417efde0
kkr <kotesh@infotech>
2025-11-02 21:23:00 -0500

    testing the message

--- src/main/java/com/sample/exercise/ExerciseApplication.java
+++ src/main/java/com/sample/exercise/ExerciseApplication.java
@@ -2,12 +2,14 @@ package com.sample.exercise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+// Testing the c//
 
 @SpringBootApplication
 public class ExerciseApplication {
 
 	public static void main(String[] args){
 		SpringApplication.run(ExerciseApplication.class, args);
+	    System.out.println("Hello World");
 	}
 	// this is test message
 	//test message 2
